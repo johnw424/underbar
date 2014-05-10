8e2ffee2e15642d5b70144dc7f3904072d4d7c0e
John Wang <johnw424@gmail.com>
2014-05-10 01:44:38 -0700

    Wrote _.each function

--- src/underbar.js
+++ src/underbar.js
@@ -54,6 +54,19 @@ var _ = {};
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      //Iterate through each index of the array
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    
+    //Only strings and arrays have the length method
+    } else if (collection.length === undefined) {
+      //Iterate through each key of the object
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
